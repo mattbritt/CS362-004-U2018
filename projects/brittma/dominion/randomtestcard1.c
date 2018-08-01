13dd0ff05472a855f1c1393f124990b2af146ef3
Matt <matt@matt-VirtualBox18.04>
2018-08-01 12:14:01 -0500

    randomcardtest1 finished and make rule implemented"

--- projects/brittma/dominion/randomtestcard1.c
+++ projects/brittma/dominion/randomtestcard1.c
@@ -7,21 +7,56 @@
  * 
  * ***********************************************/
 
-#define NOISY_TEST 1
+#define NOISY_TEST 0
 
 #include "asserttrue.h"
 #include "dominion.h"
 #include "dominion_helpers.h"
-#include "dominion.c"
+//#include "dominion.c"
 #include "rngs.h"
 #include <stdio.h>
 
 
+// count smithy cards from given array
+int smithyCount(int array[], int size)
+{
+    int count = 0;
+    for(int i = 0; i < size; i++)
+        if(array[i] == smithy) count++;
+
+    return count;
+}
+
 // verify smithy played correctly
 void checkSmithy(int currentPlayer, struct gameState *state, int handPos)
 {
+    int oSmithyCount = smithyCount(state->hand[currentPlayer], state->handCount[currentPlayer]);
+    int oHandSize = state->handCount[currentPlayer];
+    int oPlayedCardSize = state->playedCardCount;
 
-    asserttrue(playSmithy(currentPlayer, state, handPos) == 0);
+    // check that playSmithy returned 0 (success)
+    if(0 == asserttrue(playSmithy(currentPlayer, state, handPos) == 0))
+    {
+        printf("playSmithy() returned failure.\n");
+    }
+
+    // check that hand has 2 more cards (draw 3, discard the smithy)
+    if(0 == asserttrue(oHandSize == state->handCount[currentPlayer] - 2))
+    {
+        printf("hand size not 2 greater than original hand size: oHandSize: %d -- handSize: %d\n", oHandSize, state->handCount[currentPlayer]);
+    }
+
+    // check that playedCardSize has incremented by 1 (smithy was played)
+    if(0 == asserttrue(oPlayedCardSize == state->playedCardCount - 1))
+    {
+        printf("played card count not 1 greater than original count: oPlayedCardCount: %d -- playedCardCount: %d\n", oPlayedCardSize, state->playedCardCount);
+    }
+
+    // check that there is one less smithy in the hand
+    if(0 == asserttrue(oSmithyCount == smithyCount(state->hand[currentPlayer], state->handCount[currentPlayer]) + 1))
+    {
+        printf("hand does not have 1 fewer smithy: oSmithyCount: %d -- smithyCount: %d\n", oSmithyCount, smithyCount(state->hand[currentPlayer], state->handCount[currentPlayer]));
+    }
 
 
 }
